5c19835a6d0d419c2f742f3d8e15066e039aae1f
= <cassianoss@live.com>
2021-10-27 19:33:12 -0300

    fix: ajustes finais no commit anterior

---
+++ src/components/Layout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+  max-width: 617px;
+  max-height: 763px;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  margin: auto;
+  margin-top: 50px;
+  border-radius: 20px;
+  padding: 45px;
+
+
+`;
+
+export const Layout = (props) => (
+    <Styles>
+        {props.children}
+    </Styles>
+)
